679377e342b3a5c7985f1d9df6753829033ef5e9
Ruslan <70782025+digitalRM@users.noreply.github.com>
2023-06-23 20:19:17 -0700

    modal

--- components/ui/main-nav.tsx
+++ components/ui/main-nav.tsx
@@ -4,6 +4,17 @@ import { cn } from "@/lib/utils"
 
 import { MainNavContact } from "./main-nav-contact"
 
+import {
+  Dialog,
+  DialogContent,
+  DialogDescription,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
+} from "@/components/ui/dialog"
+
+
+
 
 export function MainNav({
   className,
@@ -29,7 +40,23 @@ export function MainNav({
       >
         Our Work
       </Link>
-      <MainNavContact />
+
+      <Dialog>
+        <DialogTrigger className="text-sm font-medium text-neutral-500 text-muted-foreground transition-colors hover:text-primary font-sans flex place-items-center">
+        Contact</DialogTrigger>
+        <DialogContent>
+          <DialogHeader>
+            <DialogTitle>Are you sure absolutely sure?</DialogTitle>
+            <DialogDescription>
+              This action cannot be undone. This will permanently delete your account
+              and remove your data from our servers.
+            </DialogDescription>
+          </DialogHeader>
+        </DialogContent>
+      </Dialog>
+
+
+      
 
     </nav>
   )
---
+++ components/ui/mr-video-comp.js
@@ -0,0 +1,24 @@
+import React from 'react'
+
+import {
+    Card,
+    CardContent,
+    CardDescription,
+    CardFooter,
+    CardHeader,
+    CardTitle,
+  } from "@/components/ui/card"
+
+export default function mrVideoComp() {
+  return (
+        <Card className="bg-black bg-opacity-10 border-mrStep rounded-3xl p-3 w-fit z-[6] relative my-4 cardsBreak:m-4 pb-1 cardsBreak:ml-0 h-fit smMax:h-fit flex-[1] min-w-[600px]">
+            <CardHeader className="bg-black bg-opacity-0 border-zinc-200 rounded-3xl">
+
+            </CardHeader>
+            <CardContent>
+            
+            </CardContent>
+        </Card>   
+
+  )
+}
