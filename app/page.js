87485cf340ae560c2ccf6da810a8e4461b424870
Ruslan <r1554899@gmail.com>
2023-06-27 14:15:21 -0700

    arrow fix

--- app/page.js
+++ app/page.js
@@ -158,7 +158,7 @@ export default function Home() {
           </div>
 
           
-<footer className="bg-white sm:mt-80 smMax:mt-[600px] 2xl:mt-1 dark:bg-black mt-0 ">
+<footer className="bg-white sm:mt-[16rem] smMax:mt-[600px] 2xl:mt-1 dark:bg-black mt-0 ">
     <div className="mx-auto w-full max-w-screen-2xl p-4 py-6 lg:py-8">
         <div className="md:flex md:justify-between">
           <div className="mb-6 md:mb-0">
