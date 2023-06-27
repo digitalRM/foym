09cc3bd9d64f0cac810b93434c6295b141ec28fa
Ruslan <r1554899@gmail.com>
2023-06-26 21:53:52 -0700

    fix

--- components/ui/button-work-link.tsx
+++ components/ui/button-work-link.tsx
@@ -14,7 +14,7 @@ export function ButtonWithIconWork() {
         
         </Button> */}
 
-        <a href="https://mukilteorobotics.org" className="group rounded-3xl smMax:mt-2  smMax:w-full w-full font-sans" rel="noopener noreferrer">
+        <a target="_blank" href="https://mukilteorobotics.org" className="group rounded-3xl smMax:mt-2  smMax:w-full w-full font-sans" rel="noopener noreferrer">
           <Button className="group rounded-3xl smMax:mt-2  smMax:w-full w-full font-sans" asChild>
             <div>
             Visit Website<ArrowUpRight className=' transition-all group-hover:translate-x-0.5 group-hover:-translate-y-0.5 ml-0.5'  height='16' width='16'/>
