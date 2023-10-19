8b5af7b3dfa57129df61c4dc125b1385f7ce230f
Ruslan <70782025+digitalRM@users.noreply.github.com>
2023-10-19 14:15:59 -0700

    new home branch

--- app/page.js
+++ app/page.js
@@ -156,6 +156,39 @@ export default function Home() {
 
             </div>
           </div>
+
+
+
+
+    <div className="">
+      <div className="mx-auto max-w-7xl px-6 lg:px-8">
+        <div className="mx-auto max-w-2xl sm:text-center">
+          <h2 className="text-base font-medium leading-7 bg-clip-text text-transparent bg-gradient-to-r from-brandBlue via-brandPink to-brandPink mx-52">Everything you need</h2>
+          <p className="mt-2 text-3xl font-bold tracking-tight text-white sm:text-4xl">No server? No problem.</p>
+          <p className="mt-6 text-lg leading-8 text-gray-300">
+            Lorem ipsum, dolor sit amet consectetur adipisicing elit. Maiores impedit perferendis suscipit eaque, iste
+            dolor cupiditate blanditiis.
+          </p>
+        </div>
+      </div>
+      <div className="relative overflow-hidden pt-16">
+        <div className="mx-auto max-w-7xl px-6 lg:px-8">
+          <img
+            src="https://tailwindui.com/img/component-images/dark-project-app-screenshot.png"
+            alt="App screenshot"
+            className="mb-[-12%] rounded-xl shadow-2xl ring-1 ring-white/10"
+            width={2432}
+            height={1442}
+          />
+          <div className="relative" aria-hidden="true">
+            <div className="absolute -inset-x-20 bottom-0 bg-gradient-to-t from-black pt-[7%]" />
+          </div>
+        </div>
+      </div>
+    </div>
+  
+
+
           
 <footer className="bg-white sm:mt-[16rem] smMax:mt-[600px] 2xl:mt-1 dark:bg-black mt-0 ">
     <div className="mx-auto w-full max-w-screen-2xl p-4 py-6 lg:py-8">
