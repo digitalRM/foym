bc4001893a2d76e15e8dfff0eb8e333169dab330
Ruslan <70782025+digitalRM@users.noreply.github.com>
2023-10-21 01:33:03 -0700

    link in bio

---
+++ app/bio/page.js
@@ -0,0 +1,45 @@
+// import React from 'react'
+
+// const links = [
+
+//     {
+//         title: 'Instagram',
+//         url: '#',
+//         icon: 'instagram',
+//     },
+//     {
+//         title: 'LinkedIn',
+//         url: '#',
+//         icon: 'linkedin',
+//     },
+// ]
+
+// export default function linkInBio() {
+//   return (
+//     <main className='p-8 w-screen h-screen justify-center bg-[url("./hero_join.webp")] bg-cover'>
+//         <div className='flex flex-col items-center justify-center gap-3'>
+
+//             <h1 className='text-4xl font-semibold text-center text-white'>FOYM</h1>
+
+//             {links.map((link) => (
+//                 <a
+//                     key={link.title}
+//                     href={link.url}
+//                     className='w-full gap-2 px-4 py-3 bg-neutral-900 text-white border text-center rounded-md shadow-md hover:bg-gray-100'
+//                 >
+//                     <span className='text-2xl'>
+//                         <i className={`fab fa-${link.icon}`}></i>
+//                     </span>
+//                     <span>{link.title}</span>
+//                 </a>
+//             ))
+                
+            
+//             }
+           
+
+//         </div>
+
+//     </main>
+//   )
+// }
