ad37c014ff3fd623a59527505e3206313198bbdd
Ruslan Mukhamedvaleev <r1554899@gmail.com>
2024-07-09 23:01:46 -0700

    Add portfolio

--- app/work/page.js
+++ app/work/page.js
@@ -21,6 +21,7 @@ import Image from "next/image"
 
 import mrImage from "@/public/muk.jpeg"
 import waImage from "@/public/wa.jpeg"
+import portfolioImage from "@/public/portfolio.jpeg"
 
 import { Separator } from "@/components/ui/separator"
 import { Skeleton } from "@/components/ui/skeleton"
@@ -61,7 +62,7 @@ export default function Work() {
 
             <Card className="dark:bg-blackmax-w-[1440px] bg-black rounded-[50px] p-1 w-full z-[6] relative mb-0 flex-1 sm:min-w-[477px]">
               <div className="w-full grid lg:grid-cols-2 gap-4">
-              <CardHeader className="dark:bg-black rounded-[50px] flex flex-wrap flex-row sm:gap-6 gap-3 p-3 pt-2 sm:p-12 lg:pr-0">
+              <CardHeader className="dark:bg-black rounded-[50px] flex flex-wrap flex-row sm:gap-6 gap-3 p-3 pt-2 sm:p-12 lg:pr-0 sm:pb-0">
                 <Card className="dark:bg-black w-1/3 rounded-[32px] p-1 sm:p-3 z-[6] relative my-2 mb-0 flex-1 min-w-[100%] md:min-w-[50%] flex flex-wrap">
                   <CardHeader className="dark:bg-black rounded-3xl">
                     <CardTitle className="tracking-tight text-2xl">Mukilteo Robotics</CardTitle>
@@ -94,7 +95,7 @@ export default function Work() {
                 
 
               </CardHeader>
-              <CardHeader className="dark:bg-black rounded-[50px] flex flex-wrap flex-row sm:gap-6 gap-3 p-3 pt-2 sm:p-12 lg:pl-0">
+              <CardHeader className="dark:bg-black rounded-[50px] flex flex-wrap flex-row sm:gap-6 gap-3 p-3 pt-2 sm:p-12 lg:pl-0 sm:pb-2">
                 <Card className="dark:bg-black w-1/3 rounded-[32px] p-1 sm:p-3 z-[6] relative my-2 mb-0 flex-1 min-w-[100%] md:min-w-[50%] flex flex-wrap">
                   <CardHeader className="dark:bg-black rounded-3xl">
                     <CardTitle className="tracking-tight text-2xl">Washington Youth Alliance</CardTitle>
@@ -109,6 +110,40 @@ export default function Work() {
                   
                     <Image 
                       src={waImage}
+                      priority
+                      width='100%'
+                      quality={100}
+                      placeholder="blur"
+                    
+                      height='100%'
+                      className=' rounded-2xl transition-all border border-neutral-800 mt-4'
+                      alt="Group picture of members of Mukilteo Robotics at a championship event in Washington State."
+                    /> 
+                  </div>
+                </Card>
+
+
+              
+
+                
+
+              </CardHeader>
+              <CardHeader className="dark:bg-black rounded-[50px] flex flex-wrap flex-row sm:gap-6 gap-3 p-3 sm:p-12 lg:pr-0 sm:pt-0 ">
+                <Card className="dark:bg-black w-1/3 rounded-[32px] p-1 sm:p-3 z-[6] relative my-2 mb-0 flex-1 min-w-[100%] md:min-w-[50%] flex flex-wrap">
+                  <CardHeader className="dark:bg-black rounded-3xl">
+                    <CardTitle className="tracking-tight text-2xl">The Portfolio - An FOYM<span className="text-brandPink lg:text-4xl">.</span> Template</CardTitle>
+                    <CardDescription className=" mt-3 " >This is the first template we have created for our clients—the first of many to come. It is a simple, clean, modern website modeled after a resume, perfect for showcasing your work. It is fully responsive and works on all devices. It is also fully customizable and easy to use. You can use it to create a portfolio that showcases your work to potential clients and employers. </CardDescription>
+                  </CardHeader>
+                  <div className="flex w-full max-w-[100%] items-center sm:space-x-2 z-20 p-2 m-4 mt-6 pt-0 pl-1 mb-2  relative smMax:flex-wrap">
+                    <ButtonWithIconWork link="https://portfolio.foym.org" />
+                    <ButtonWithIconWork link="https://github.com/digitalRM/FOYM-Portfolio" color="code" />
+                  </div>  
+
+                  <div className="group w-full max-w-[100%] items-center sm:space-x-2 z-20 p-2 m-4 mt-0 pt-0 pl-1 mb-2  relative smMax:flex-wrap">
+                    {/* <Badge variant='outline' className=" g ml-1 translate-y-7 translate-x-4 font-sans  opacity-100 relative z-10 border-zinc-600 cursor-pointer">View More</Badge> */}
+                  
+                    <Image 
+                      src={portfolioImage}
                       priority
                       width='100%'
                       quality={100}
--- components/ui/button-work-link.tsx
+++ components/ui/button-work-link.tsx
@@ -6,9 +6,30 @@ import Layout from "feather-icons-react/build/IconComponents/Layout"
 import { ArrowUpRight } from "lucide-react"
 import Link from "next/link"
 
-export function ButtonWithIconWork({link}) {
+export function ButtonWithIconWork({link, color}) {
+
+  if(color === 'code') {
   return (
     <>
+    {/* <Button className="group rounded-3xl smMax:mt-2  smMax:w-full w-full font-sans">
+      Learn More<ArrowUpRight className=' transition-all group-hover:translate-x-0.5 group-hover:-translate-y-0.5 ml-0.5'  height='16' width='16'/>
+    
+    </Button> */}
+
+    <a target="_blank" href={link} className="group rounded-3xl smMax:mt-2  smMax:w-full w-full font-sans" rel="noopener noreferrer">
+      <Button variant="outline" className="group rounded-3xl smMax:mt-2  smMax:w-full w-full font-sans" asChild>
+        <div>
+        View Code<ArrowUpRight className=' transition-all group-hover:translate-x-0.5 group-hover:-translate-y-0.5 ml-0.5'  height='16' width='16'/>
+        </div>
+          
+      </Button>
+    </a>
+
+    
+</>
+  )} else {
+    return (
+      <>
         {/* <Button className="group rounded-3xl smMax:mt-2  smMax:w-full w-full font-sans">
           Learn More<ArrowUpRight className=' transition-all group-hover:translate-x-0.5 group-hover:-translate-y-0.5 ml-0.5'  height='16' width='16'/>
         
@@ -25,5 +46,7 @@ export function ButtonWithIconWork({link}) {
 
         
     </>
-  )
+    )
+  }
+
 }
